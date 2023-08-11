39af5ebd186b0090d884124a2828a72586b55913
Omer Sezer <omerysezer@gmail.com>
2023-08-11 16:58:49 -0400

    animation controller now runs animation files

--- src/Animations/animation_controller.py
+++ src/Animations/animation_controller.py
@@ -3,32 +3,34 @@ import os
 from time import time, sleep
 
 class AnimationController:
-    def __init__(self, animation_names, animation_runtime, controller_to_animation_queue, animation_to_controller_queue, kill_sentinel):
+    def __init__(self, animation_names, animation_runtime, controller_to_animation_queue, animation_to_controller_queue, kill_sentinel, led_strip):
         self.controller_to_animation_queue = controller_to_animation_queue
         self.animation_to_controller_queue = animation_to_controller_queue
         self.kill_sentinel = kill_sentinel
 
-        self.animation_time = animation_runtime
+        self.lights = led_strip
+        self.num_pixels = self.lights.get_pixel_count()
+
+        self.animation_time_ms = animation_runtime * 1000
 
         self.animations = []
         self.animation_loads_failed = []
-
-        # to prevent NoneType is not iterable error
+        self.animation_idx = 0
+        
         animation_names = animation_names or []
         for animation_name in animation_names:
             try:   
                 animation = getattr(import_module(f"src.Animations.LightAnimations.{animation_name}", package="."), animation_name)
-                self.animations.append(animation(None))
+                self.animations.append(animation(self.lights, self.num_pixels))
             except Exception as e:
                 self.animation_loads_failed.append(animation_name)
                 print(e)
-        
+
     def get_classes_which_failed_to_load(self):
         return self.animation_loads_failed
     
     def run(self):
-        animation_index = 0
-        time_of_animation_start = None
+        time_of_animation_start_ms = float('-inf')
         while True and self.animations:
             if not self.controller_to_animation_queue.empty():
                 message = self.controller_to_animation_queue.get()
@@ -37,10 +39,10 @@ class AnimationController:
                     return
             
             # move on to the next animation if time for current animation is up or we have not started any animation yet
-            current_time = int(time() * 1000) 
-            if not time_of_animation_start or current_time - time_of_animation_start > self.animation_time:
-                time_of_animation_start = current_time
-                animation_index = (animation_index + 1) % len(self.animations)
+            current_time_ms = int(time() * 1000) 
+            if current_time_ms - time_of_animation_start_ms > self.animation_time_ms:
+                time_of_animation_start_ms = current_time_ms
+                self.animation_idx = (self.animation_idx + 1) % len(self.animations)
             
-            self.animations[animation_index].run()
-            sleep(.01)
+            self.animations[self.animation_idx].run()
+            sleep(.005)
