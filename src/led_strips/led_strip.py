c197eb0f283b5a5eceef605c7d5fa79c935aaac4
flammingostrich <omerysezer@gmail.com>
2023-09-07 22:31:59 +0100

    implemented changing brightness

--- src/led_strips/led_strip.py
+++ src/led_strips/led_strip.py
@@ -9,13 +9,11 @@ class LED_STRIP:
                 num_led (int): the number of pixels on the led strip
                 strip_type (["neopixel", "dotstar"]): The type of led strip being used
         '''
-
         if strip_type not in ["neopixel", "dotstar"]:
             raise Exception(f"Unsupported strip type: '{strip_type}'. Strip must be of type either 'neopixel' or 'dotstar'.")
-        
-        self._strip = None
+
         if strip_type == "neopixel":
-            self._strip = neopixel.NeoPixel(pin=board.D10, n=num_led, brightness=brightness, auto_write=False, pixel_order="PRGB")
+            self._strip = neopixel.NeoPixel(pin=board.D18, n=num_led, brightness=brightness, auto_write=False, pixel_order=neopixel.GRB)
         else:
             # adafruit_dotstar.BGR is used because BGR and RGB are backwards for some reason
             self._strip = adafruit_dotstar.DotStar(clock=board.D11, data=board.D10, n=num_led, brightness=brightness, auto_write=False, pixel_order=adafruit_dotstar.BGR, baudrate=8000000)
@@ -23,6 +21,7 @@ class LED_STRIP:
         self.num_led = num_led
         self.brightness = brightness
         self.light_lock = threading.Lock()
+        self._pixel_values = [(0,0,0)] * num_led
 
         # reset lights to black before beginning
         for i in range(self.num_led):
@@ -36,15 +35,14 @@ class LED_STRIP:
 
     def fill_all(self, r, g, b):
         # doesnt need to have lock because self.fill calls set_pixel which acquires the lock
-        self.light_lock.acquire()
-        self._strip.fill((r, g, b))
-        self.light_lock.release()
+        self.fill(0, self.num_led, r, g, b)
 
     def set_pixel(self, i, r, g, b):
         # we keep light brightness at 100% and adjust pixel values ourselves
-        # changing brightness is expensive for the library 
+        # changing brightness is expensive for the library
         self.light_lock.acquire()
-        self._strip[i] = (r,g,b)
+        self._pixel_values[i] = (r,g,b)
+        self._strip[i] = tuple([round(channel * self.brightness) for channel in (r,g,b)])
         self.light_lock.release()
 
     def set_brightness(self, brightness):
@@ -52,8 +50,11 @@ class LED_STRIP:
         Takes a brightnes value from [0, 100]
         '''
         self.light_lock.acquire()
-        self._strip.brightness = brightness/100
+        self.brightness = brightness/100
         self.light_lock.release()
+        for i in range(self.num_led):
+           self.set_pixel(i, *self._pixel_values[i])
+        self.show()
 
     def show(self):
         self.light_lock.acquire()
